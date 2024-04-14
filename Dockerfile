50f47453d80bf1173d09166931382bda5eb3fbbe
love-edx <l.tyagi@edexa.team>
2024-04-14 18:59:12 +0530

    chore: docker expose for ports

--- Dockerfile
+++ Dockerfile
@@ -5,13 +5,14 @@ FROM node:16-alpine
 WORKDIR /app
 
 # Copy package.json and package-lock.json to the /app directory
-COPY package.json package-lock.json ./
+COPY package*.json ./
 
 # Install dependencies
 RUN npm install
 
 # Copy the rest of project files into this image
 COPY . .
+
 
 # Expose application port
 EXPOSE 3000
--- app.ts
+++ app.ts
@@ -1,17 +1,58 @@
+// app.ts
 import express from 'express';
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+
+// This error should crash whole process
+const envFound = dotenv.config();
+if (!envFound) throw new Error('⚠️ Couldn not find .env file ⚠️');
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
-async function helloWorld() {
-  return { status: 200, message: 'Github PR strict action' };
-}
-
-app.get('/', async (req, res) => {
-  return res.status(200).send({ status: 200, message: 'Hello world!' });
-});
-
-app.listen(PORT, () =>
-  console.log(`⚡Server is running here 👉 http://localhost:${PORT}`)
+console.log(
+  process.env.DB_USER,
+  process.env.DB_HOST,
+  process.env.DB_NAME,
+  process.env.DB_PASSWORD,
+  process.env.DB_PORT
 );
 
-export { helloWorld };
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || '5432'),
+});
+
+// Connect to PostgreSQL
+client
+  .connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL');
+  })
+  .catch((error) => {
+    console.error('Error connecting to PostgreSQL:', error);
+    process.exit(1); // Exit process on connection error
+  });
+
+app.get('/', async (req, res) => {
+  // Connect to PostgreSQL
+  try {
+    // Perform a simple query to PostgreSQL
+    const result = await client.query('SELECT NOW()');
+
+    console.log('Query result:', result.rows);
+
+    // Output the result
+    res.status(200).send(result.rows[0]);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
